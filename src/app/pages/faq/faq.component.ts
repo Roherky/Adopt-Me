ecd6bb6bb5548146a3c1e4e5883a2f350f7b5ec6
PatriB20 <patriciabatallaarias@gmail.com>
2021-12-05 17:02:26 +0100

    modificaciones2

--- src/app/pages/faq/faq.component.ts
+++ src/app/pages/faq/faq.component.ts
@@ -6,8 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./faq.component.css']
 })
 export class FaqComponent implements OnInit {
+  public mostrar: Boolean;
+  constructor() { 
+    this.mostrar=false;
+  }
+  public ver(){
 
-  constructor() { }
+      this.mostrar=!this.mostrar
+
+    }
 
   ngOnInit(): void {
   }
