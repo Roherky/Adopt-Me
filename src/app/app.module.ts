a45de45b9eadcd2471c5b31b79aee752339c6a2f
StefaniaBuri <stefania_0506@hotmail.com>
2021-12-09 18:33:05 +0100

    Actualizado module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { HttpClient, HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
@@ -87,7 +87,9 @@ import { MatFormFieldModule } from '@angular/material/form-field';
     MatCheckboxModule,
     ReactiveFormsModule,
     NgxMatFileInputModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    HttpClientModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
