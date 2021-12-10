f5928557762092f307857ccc9f7d27e87065ce9c
Roherky <rodrigo31996@gmail.com>
2021-12-10 16:44:00 +0100

    Login hecho

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Login } from 'src/app/models/login';
 import { Protectora } from 'src/app/models/protectora';
 import { Usuario } from 'src/app/models/usuario';
+import { SesionesService } from 'src/app/shared/sesiones.service';
 
 @Component({
   selector: 'app-login',
@@ -12,14 +14,22 @@ export class LoginComponent implements OnInit {
 
   public usuario: Usuario;
   public protectora: Protectora;
+  public login: Login;
 
-  constructor() {
+  constructor(private loginService: SesionesService) {
     this.usuario = new Usuario(0, "", "", "", 0, "", "", "", "", "");
     this.protectora = new Protectora(0,"", "", "", "", "", "","");
   }
 
-  onSubmit(form: NgForm){
-    console.log(this.usuario);
+  hacerLogin(email: string, password: string){
+    console.log(email);
+    console.log(password);
+    let object = new Login(0, 0, 0, email, password);
+    this.loginService.postLogin(object).subscribe((data: any) => {
+      console.log(data);
+      if(data != "-1") console.log("Se ha creado el usuario con ID: " + data + " satisfactoriamente");
+      else console.log("Ha ocurrido un error al procesar su solicitud");
+    })
   }
 
   ngOnInit(): void {
