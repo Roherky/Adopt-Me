7a3dd684e6d8175819a211d48c272550a0b57b0e
Roherky <rodrigo31996@gmail.com>
2021-12-12 16:20:57 +0100

    Funcionalidad finales felices

--- src/app/pages/finales-felices/finales-felices.component.ts
+++ src/app/pages/finales-felices/finales-felices.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { FinalFeliz } from 'src/app/models/final-feliz';
 import { FinalFelizService } from 'src/app/shared/final-feliz.service';
 
@@ -11,27 +12,50 @@ export class FinalesFelicesComponent implements OnInit {
 
   public finalFeliz: FinalFeliz;
   public finalesFelices: FinalFeliz[];
+  closeResult = '';
 
-  constructor(private apiService: FinalFelizService) {
+  constructor(private finalFelizService: FinalFelizService, private modalService: NgbModal) {
     this.finalFeliz = new FinalFeliz(0, "", "", "", "", 0);
   }
 
-  public mostrarFinalesFelices(id: any){
-    if(id != ""){
-      this.apiService.getFinalFeliz(id).subscribe((data: any) => {
-        this.finalesFelices = [data[0]];
-        console.log(data);
-      })
-    }
-    else {
-      this.apiService.getFinalesFelices().subscribe((data: any) => {
-        this.finalesFelices = data;
-        console.log(this.finalesFelices);
-      })
+  // public mostrarFinalesFelices(id: any){
+  //   if(id != ""){
+  //     this.apiService.getFinalFeliz(id).subscribe((data: any) => {
+  //       this.finalesFelices = [data[0]];
+  //       console.log(data);
+  //     })
+  //   }
+  //   else {
+  //     this.apiService.getFinalesFelices().subscribe((data: any) => {
+  //       this.finalesFelices = data;
+  //       console.log(this.finalesFelices);
+  //     })
+  //   }
+  // }
+
+  abrirModal(content: any){
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
     }
   }
 
   ngOnInit(): void {
+    this.finalFelizService.getFinalesFelices().subscribe((data: FinalFeliz[]) => {
+      this.finalesFelices = data;
+      console.log(this.finalesFelices);
+    })
   }
 
 }
